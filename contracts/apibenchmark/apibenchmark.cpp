d5b0315469104e8002c1ed5ea05d9db9e0ba5a63
Igor Lins e Silva <igor@eosrio.io>
2019-10-04 05:24:46 -0300

    Update apibenchmark.cpp

--- contracts/apibenchmark/apibenchmark.cpp
+++ contracts/apibenchmark/apibenchmark.cpp
@@ -25,6 +25,25 @@ CONTRACT apibenchmark : public eosio::contract {
             uint16_t elapsed;
         };
 
+        struct [[eosio::table, eosio::contract("eosio.system")]] producer_info {
+            name                  owner;
+            double                total_votes = 0;
+            eosio::public_key     producer_key; /// a packed public key object
+            bool                  is_active = true;
+            std::string           url;
+            uint32_t              unpaid_blocks = 0;
+            time_point            last_claim_time;
+            uint16_t              location = 0;
+            uint64_t primary_key()const { return owner.value;                             }
+            double   by_votes()const    { return is_active ? -total_votes : total_votes;  }
+            bool     active()const      { return is_active;                               }
+            void     deactivate()       { producer_key = public_key(); is_active = false; }
+
+            EOSLIB_SERIALIZE(producer_info,(owner)(total_votes)(producer_key)(is_active)(url)(unpaid_blocks)(last_claim_time)(location) )
+        };
+
+        typedef eosio::multi_index<"producers"_n,producer_info,indexed_by<"prototalvote"_n, const_mem_fun<producer_info, double, &producer_info::by_votes>>>producers_table;
+
         /**
          * Simple CPU benchmark that calculates Mersenne prime numbers.
          */
@@ -158,26 +177,6 @@ CONTRACT apibenchmark : public eosio::contract {
         }
 
     private:
-
-        struct [[eosio::table, eosio::contract("eosio.system")]] producer_info {
-              name                  owner;
-              double                total_votes = 0;
-              eosio::public_key            producer_key;
-              bool                  is_active = true;
-              std::string           url;
-              uint32_t              unpaid_blocks = 0;
-              time_point            last_claim_time;
-              uint16_t              location = 0;
-
-              uint64_t primary_key()const { return owner.value;                             }
-              double   by_votes()const    { return is_active ? -total_votes : total_votes;  }
-              bool     active()const      { return is_active;                               }
-              void     deactivate()       { producer_key = public_key(); is_active = false; }
-
-              EOSLIB_SERIALIZE(producer_info,(owner)(total_votes)(producer_key)(is_active)(url)(unpaid_blocks)(last_claim_time)(location) )
-        };
-
-        typedef eosio::multi_index<"producers"_n,producer_info,indexed_by<"prototalvote"_n, const_mem_fun<producer_info, double, &producer_info::by_votes>>>producers_table;
 
         struct measurement {
             name tester;
